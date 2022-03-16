2c8e7f554f3e4d4fbd0ca37997d46324d8125bf9
adam alsaid kamal <adamzarzora9@gmail.com>
2022-03-16 20:32:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ let budget = 20;
 console.log(budget >= 30 && budget <= 80);
 budget += 30;
 console.log(budget >= 30 && budget <= 80);
+//3rd repo
+let grade = prompt("ادخل درجتك");
+switch (grade) {
+  case grade >= 90:
+    console.log("لقد حصلت على امتياز 🥳");
+    break;
+  case grade >= 80:
+    console.log("لقد حصلت على جيد جداً🤩");
+    break;
+  case grade >= 70:
+    console.log("لقد حصلت على جيد🙂");
+    break;
+  case grade >= 60:
+    console.log("لقد حصلت على مقبول😕");
+    break;
+  case grade >= 50:
+    console.log("لقد حصلت على ضعيف☹️");
+    break;
+  case grade < 50:
+    console.log("راسب💔");
+}
