e2abe843bbe884f7fbc61c5342654d81db63fb6e
adam alsaid kamal <adamzarzora9@gmail.com>
2022-03-16 19:35:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 alert("adam alsaid");
-console.log(14 - 15, "color: orange");
+console.log(14 - 15);
 console.log("%ci am in ninth grade", "color: red");
 /*java isn't javascript */
+let budget = 20;
+console.log(budget >= 30 && budget <= 80);
+budget += 30;
+console.log(budget >= 30 && budget <= 80);
